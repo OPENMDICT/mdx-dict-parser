50348d0fcb94f20c3ea1e3fb29a09e13bd35e70f
PrinOrange <prinorange@outlook.com>
2024-12-11 22:48:04 +0800

    chores: add cargo deps.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![deny(clippy::all)]
+use mdict_parser::{parser, mdict::Record};
 
 #[macro_use]
 extern crate napi_derive;
@@ -6,4 +7,20 @@ extern crate napi_derive;
 #[napi]
 pub fn sum(a: i32, b: i32) -> i32 {
   a + b
+}
+
+#[napi]
+pub fn parse(){
+  // let input = include_bytes!("../dictionary.mdx");
+  // let dict = parser::parse(input);
+
+  // // iter dictionary entries
+  // for key in dict.keys() {
+  //   println!("{:?}", item)
+  // }
+
+  // // iter all dictionary records
+  // for item in dict.items() {
+  //   println!("{:?}", item)
+  // }
 }
